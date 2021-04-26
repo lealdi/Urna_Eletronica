24802586ee8e3a09aead26e46b0cbd1848a60a10
lealdi <diegoleal1913@gmail.com>
2021-04-26 04:54:21 -0300

    Updates

---
+++ js/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+        {
+            numero: 38111,
+            nome: "Fulano de tal",
+            partido: "ABC",
+            fotos: [
+                {url:"political1.jpg", legenda: "Vereador"}
+            ]
+        },
+        {
+            numero: 77222,
+            nome: "Beltrano da Silva",
+            partido: "DEFG",
+            fotos: [
+                {url: "political1.jpg", legenda: "Vereador"}
+            ]
+        },
+      ]
+    },
+    {
+        titulo: "PREFEITO",
+        numero: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: "Ciclano",
+                partido: "ABC",
+                vice: "Cic",
+                fotos:[
+                    {url: "political.jpg", legenda: "Prefeito"},
+                    {url: "political.jpg", legenda: "Vice-Prefeito", small:true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: "Zulano",
+                partido: "QWERTY",
+                vice: "Zul",
+                fotos:[
+                    {url: "political.jpg", legenda: "Prefeito"},
+                    {url: "political.jpg", legenda: "Vice-Prefeito", small:true}
+                ]
+            }
+        ]
+    }
+];
+
